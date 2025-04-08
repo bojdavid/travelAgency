6807fee75c3eb70e65505c4cdc2c29f5d726bb17
bojdavid <bojdavid80@gmail.com>
2025-04-08 18:05:39 +0100

    minor refinements

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,19 +15,32 @@
   ];
 </script>
 
-<header class="mt-10">
-  <nav class="bg-stone-950 text-stone-100 w-xl mx-auto rounded-full p-5 flex">
-    <ul class="flex justify-evenly items-center">
-      {#each navs as nav}
-        <li class="capitalize text-lg">
-          <a href={nav.url}>
-            {nav.nav}
-          </a>
-        </li>
-      {/each}
-    </ul>
-    <a href="/bookATrip" class="p-4 bg-stone-300 justify-end">Book A Trip</a>
-  </nav>
+<header class="mt-10 font-hanken">
+  <div class="flex justify-center">
+    <div class="w-[90px]">
+      <p class="text-[24px] mx-auto font-sofia font-weight-400">
+        Soar (::) Travels
+      </p>
+    </div>
+    <nav
+      class="bg-stone-950 text-stone-100 w-[540px] rounded-full py-[7px] pr-[6px] pl-[20px] flex justify-between text-lg"
+    >
+      <ul class="flex justify-evenly gap-5 items-center">
+        {#each navs as nav}
+          <li class="">
+            <a href={nav.url}>
+              {nav.nav}
+            </a>
+          </li>
+        {/each}
+      </ul>
+      <a
+        href="/bookATrip"
+        class="py-2 px-6 bg-stone-50 justify-end text-stone-950 rounded-full"
+        ><span class="m-0"> Book a trip </span>
+      </a>
+    </nav>
+  </div>
 </header>
 <main>
   {@render children()}
