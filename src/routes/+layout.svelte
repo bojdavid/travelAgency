5c4bfb30c39c0ade48e6bb8c857eda50086b362f
bojdavid <bojdavid80@gmail.com>
2025-04-08 06:50:46 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import "../lib/styles/app.css";
+  let { children } = $props();
+</script>
+
+<header></header>
+<main>
+  {@render children()}
+</main>
+<footer>footer</footer>
